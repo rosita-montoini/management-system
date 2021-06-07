ab1f5efa29466840c8e9e9d6b3ad12bcb99aad29
Rosita Montoini <r.montoini@gmail.com>
2021-06-07 15:56:30 +0300

    Changed

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,12 @@ import { useAuth } from './hooks/useAuth';
 import { AuthContext } from './context/authContext';
 import { Navbar } from './components/navbar/navbar';
 import { Loader } from '../../components/loader/loader';
+import { useHttp } from './hooks/useHttp';
 import 'materialize-css';
 
 function App() {
   const { token, login, logout, userId } = useAuth();
+  const { loading } = useHttp();
   const isAuth = !!token;
   const routes = useRoutes(isAuth);
 
