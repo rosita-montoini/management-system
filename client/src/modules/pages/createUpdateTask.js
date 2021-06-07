16fe1b058fcde97141ee193d08d3bc683f67b985
Rosita Montoini <r.montoini@gmail.com>
2021-06-07 18:29:10 +0300

    some changed

--- client/src/modules/pages/createUpdateTask.js
+++ client/src/modules/pages/createUpdateTask.js
@@ -10,7 +10,6 @@ import {
 import CloseIcon from '@material-ui/icons/Close';
 import { useHttp } from '../../hooks/useHttp';
 import { AuthContext } from '../../context/authContext';
-import { useHistory } from 'react-router-dom';
 
 const NUMBERS_REG = /^[1-9]\d*$/;
 
@@ -47,7 +46,6 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export const CreateUpdateTask = ({ handleClose, taskById }) => {
-    const history = useHistory();
     const styles = useStyles();
     const authContext = useContext(AuthContext);
     const { request } = useHttp();
@@ -59,8 +57,7 @@ export const CreateUpdateTask = ({ handleClose, taskById }) => {
         try {
             await request('/task/edit', 'POST', {id, ...values}, {
                 Authorization: `Bearer ${authContext.token}`
-            });
-            history.goBack();
+            }).then(window.location.assign('https://app-manag-system.herokuapp.com/task'));
         } catch (err) {}
     };
 
