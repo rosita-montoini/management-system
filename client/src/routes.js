f5ae7d06413a9eaf19e55dea008e9b323bd867f5
Rosita Montoini <r.montoini@gmail.com>
2021-06-29 13:08:06 +0300

    changed router

--- client/src/routes.js
+++ client/src/routes.js
@@ -12,7 +12,7 @@ export const useRoutes = isAuthenticated => {
                 <Switch>
                     <Route path="/task" component={HomePage} exact />
                     <Route path="/sometask" component={TaskDetailsPage} exact />
-                    <Redirect to="/task"/>
+                    <Redirect from="/task" to="/"/>
                 </Switch>
             </Router>
         );
